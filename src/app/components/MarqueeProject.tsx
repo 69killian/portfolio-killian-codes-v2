38d2fa8c49b7456b61c1f62a3c06f851db9ef584
Killian <martykillian7@gmail.com>
2025-04-27 10:48:08 +0200

    adding new Resme and new projects

--- src/app/components/MarqueeProject.tsx
+++ src/app/components/MarqueeProject.tsx
@@ -3,14 +3,11 @@ import { useEffect } from 'react';
 import Image from 'next/image';
 import stripint from "../../../public/black.png";
 import arcadia from "../../../public/arcadia.png";
-import shoesea from "../../../public/shoesea.png";
+import gitify from "../../../public/gitify.png";
 import admina from "../../../public/admina.png";
 import modea from "../../../public/modea.png";
-import cards from "../../../public/cards.png";
-import Pricing from "../../../public/pricing.png";
+import trackhub from "../../../public/trackhub.png";
 import AOS from 'aos';
-import Paste from '../../../public/Paste.png';
-import Wallify from '../../../public/wallify.png';
 import 'aos/dist/aos.css';
 
 const MarqueeProject = () => {
@@ -24,15 +21,12 @@ const MarqueeProject = () => {
       {/* Images défilantes organisées en colonnes */}
       <div data-aos="fade-up" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 p-10 gap-8 items-center justify-center text-center">
         {[
-          { src: stripint, alt: "Stripint, Intégration Stripe", link: "https://landing-page-stripe-subscription-two.vercel.app/" },
-          { src: admina, alt: "Admina", link: "https://just-branding-655225.framer.app/" },
-          { src: Paste, alt: "Paste", link: "https://mighty-travel-887542.framer.app/" },
-          { src: Wallify, alt: "Wallify Landing Page", link: "https://creative-points-743034.framer.app/" },
-          { src: Pricing, alt: "Pricing Landing Page", link: "https://modern-landing-page-pricing-vuejs.vercel.app/" },
-          { src: arcadia, alt: "Arcadia, Zoo de Bretagne", link: "https://github.com/69killian/Arcadia-Zoo" },
-          { src: modea, alt: "Modea", link: "https://modea-e-commerce-full-stack-app-store.vercel.app/" },
-          { src: cards, alt: "Landing page cards", link: "https://landing-page-card-product.vercel.app/" },
-          { src: shoesea, alt: "Shoesea, Site de chaussures en ligne", link: "https://shoes-ecommerce-tailwind.vercel.app/" },
+                    { src: gitify, alt: "Gitify, Challenges GitHub", link: "https://warmhearted-imagine-949567.framer.app/" },
+                    { src: trackhub, alt: "TrackHub, Dashboard de suivi d'habitudes", link: "https://track-hub-seven.vercel.app/" },
+                    { src: stripint, alt: "Stripint, Intégration Stripe", link: "https://landing-page-stripe-subscription-two.vercel.app/" },
+                    { src: admina, alt: "Admina", link: "https://just-branding-655225.framer.app/" },
+                    { src: arcadia, alt: "Arcadia, Zoo de Bretagne", link: "https://github.com/69killian/Arcadia-Zoo" },
+                    { src: modea, alt: "Modea", link: "https://modea-e-commerce-full-stack-app-store.vercel.app/" },
         ].map((image, index) => (
           <div key={index} data-aos="fade-up" className="relative group">
             {/* Envelopper l'image avec un lien */}
